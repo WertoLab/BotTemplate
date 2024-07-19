097592ff6dd1ed973e3e9c8bd8f6a8aa0799fbaf
W9JAK <112979587+W9JAK@users.noreply.github.com>
2024-07-19 14:20:49 +0300

    fix keyboard

--- src/keyboards/delete_paper_keyboard.py
+++ src/keyboards/delete_paper_keyboard.py
@@ -1,8 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_delete_paper_keyboard(papers):
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    buttons = [
         [InlineKeyboardButton(f"Удалить '{paper.title}'", callback_data=f"delete_paper:{paper.id}")]
         for paper in papers
-    ])
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     return keyboard
